c50653a276ea0b68e5f23bb594349a938ed483be
Doker123 <dodo2larin@gmail.com>
2026-06-22 09:28:16 +0700

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    "driver" => "mysql",
+    "host" => "localhost",
+    "port" => 3306,
+    "database" => "s3r4duex_m2",
+    "username" => "s3r4duex",
+    "password" => "oyyqwF02",
+    "charset" => "utf8mb4",
+];
